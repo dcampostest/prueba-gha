c003a691a8f704b5c04448bb10608fbd5ab6944d
Darian Campos <darian.campos@peedidosya.com>
2023-03-08 17:46:13 -0300

    fix pr.yml and add code generate file main.py file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+# import module
+from datetime import datetime
+  
+# get current date and time
+current_datetime = datetime.now()
+print("Current date & time : ", current_datetime)
+  
+# convert datetime obj to string
+str_current_datetime = str(current_datetime)
+  
+# create a file object along with extension
+file_name = str_current_datetime+".txt"
+file = open(file_name, 'w')
+  
+print("File created : ", file.name)
+file.close()
